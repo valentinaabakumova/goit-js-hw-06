cc3fc5c04bf936ad4e8e563f05d6fe3b4eea11f4
valentinaabakumova <abakumova.va13@gmail.com>
2022-05-13 11:20:20 +0200

    try task 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,11 +7,48 @@ const controls = document.querySelector("#controls");
 // let amount = controls.firstChild.textContent;
 const butCreate = document.querySelector("button[data-create]");
 const butDestroy = document.querySelector("button[data-destroy]");
+let amount = 6;
+let myArr = [];
 
-butCreate.addEventListener("click", () => {
-  console.log("gogogo");
+const createArray = (amount) => {
+  for (let i = 0; i < amount; i += 1) {
+    myArr.push((i + 3) * 10);
+  }
+  console.log(myArr);
+  return myArr;
+};
+
+createArray(amount);
+
+const createBox = () => {
+  return `
+   <div class="boxEl">
+box
+  </div>
+  `;
+};
+
+butCreate.addEventListener("click", (amount, myArr) => {
+  // console.log("gogogo");
+
+  for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+
+    const el = document.createElement("div");
+    el.classList.add("boxEl");
+
+    box.append(el);
+    console.log("Amount: ", amount);
+  }
+
+  // for (let index = 0; index < amount; index++) {
+  //   console.log(index);
+  //   box.insertAdjacentHTML("beforeend", createBox());
+  // }
 });
 
 butDestroy.addEventListener("click", () => {
   console.log("oooo");
+  //// что-то не так
+  box.remove();
 });
