ed651051c01b0ed421240b0b388e1ba847de197b
valentinaabakumova <abakumova.va13@gmail.com>
2022-05-13 11:35:58 +0200

    task5, second option

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,19 @@
 let nameIn = document.querySelector("#name-input");
 let nameOut = document.querySelector("#name-output");
 
-nameIn.addEventListener("input", () => {
-  if (nameIn.value != "") {
-    nameOut.textContent = nameIn.value;
+// nameIn.addEventListener("input", () => {
+//   if (nameIn.value != "") {
+//     nameOut.textContent = nameIn.value;
+//   } else {
+//     nameOut.textContent = "Anonymous";
+//   }
+// });
+
+/////// second option
+
+nameIn.addEventListener("input", (event) => {
+  if (event.currentTarget.value != "") {
+    nameOut.textContent = event.currentTarget.value;
   } else {
     nameOut.textContent = "Anonymous";
   }
